85f6860460b55fb77f16fb235ebbc08206991ee2
Patryk Jaworski <pjaworski.dev@gmail.com>
2021-11-02 00:03:02 +0100

    Fix mobile UI

--- script.js
+++ script.js
@@ -67,3 +67,72 @@ particlesJS("particles-js", {
   });
 
 
+  particlesJS("particles-js-mobile", {
+    "particles": {
+      "number": {
+        "value": 10,
+        "density": {
+          "enable": true,
+          "value_area":700
+        }
+      },
+      "size": {
+        "value": 0,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 450,
+        "color": "#858585",
+        "opacity": 0.4,
+        "width": 3
+      },
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": false
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 0,
+          "line_linked": {
+            "opacity": 0
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 300,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+
+
