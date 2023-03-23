74e0e469558e6a3c766c710549437a121e2a2668
P Jaworski <pjaworski.dev@gmail.com>
2023-03-24 00:13:24 +0100

    Added routes, etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 const App = () => {
  return (
   <div className="app_content">
    <Hero />
+   <Skills/>
    <Projects />
   </div>
  );
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes/publicRoutes";
+import "./styles/index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+ <React.StrictMode>
+  <RouterProvider router={routes} />
+ </React.StrictMode>
+);
