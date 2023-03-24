256e52365aa0155d8a28cd76bc6db125910426b6
P Jaworski <pjaworski.dev@gmail.com>
2023-03-24 01:28:10 +0100

    Skills sect improvements

--- src/components/UI/SkillsItem.jsx
+++ src/components/UI/SkillsItem.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const SkillsItem = ({children, title}) => {
+const SkillsItem = ({children, title, bigger_style}) => {
  return (
-  <div className="skills__item">
+  <div className={`skills__item ${bigger_style && 'skills__item__bigger'}`}>
    <div className="skills__item_content">
     <h3 className="skills__title">{title}</h3>
     <div className="skills__icon">
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/publicRoutes";
 import "./styles/index.css";
+import "./styles/media.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
  <React.StrictMode>
