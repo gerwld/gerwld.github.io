d410de3d4bdfb7359fd5cba44b2a187646cdcfde
P Jaworski <pjaworski.dev@gmail.com>
2023-03-31 19:13:44 +0200

    Added img cache, fix links

--- src/components/UI/ProjectItem.jsx
+++ src/components/UI/ProjectItem.jsx
@@ -1,7 +1,10 @@
 import { AiOutlineGithub } from "react-icons/ai";
 import { HiOutlineExternalLink } from "react-icons/hi";
+import { useImgCache } from "../../hooks/useImgCache";
 
 const ProjectItem = ({ title, desc, img, source_url, preview_url }) => {
+  useImgCache([img]);
+
  return (
   <article className="projects__item">
    <div className="projects__group">
@@ -9,8 +12,15 @@ const ProjectItem = ({ title, desc, img, source_url, preview_url }) => {
     <p className="project__desc">{desc}</p>
 
     <div className="project__links">
-      <a href={preview_url} className={preview_url?.length ? '' : 'inactive'} target="_blank" rel="noopener"><HiOutlineExternalLink/>Live demo</a>
-      <a href={source_url} className={source_url?.length ? '' : 'inactive'} target="_blank" rel="noopener"><AiOutlineGithub/>View source</a>
+      {preview_url?.length ? 
+        <a href={preview_url}  target="_blank" rel="noopener"><HiOutlineExternalLink/>Live demo</a>
+        : <span className='inactive' target="_blank" rel="noopener"><HiOutlineExternalLink/>Live demo</span>
+      }
+      
+      {source_url?.length ?
+        <a href={source_url} target="_blank" rel="noopener"><AiOutlineGithub/>View source</a>
+        : <span className='inactive' target="_blank" rel="noopener"><AiOutlineGithub/>View source</span>
+      }
     </div>
    </div>
    <div className="projects__img">
