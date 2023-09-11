eca6e6bbf6b4999c04e271afc5a401e17498fe1b
P Jaworski <pjaworski.dev@gmail.com>
2023-09-12 01:43:42 +0200

    Updages, part 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,25 @@ import { Hero, MainLoader, Navbar, Projects, Skills } from "./components/";
 import { setInit } from "./redux/reducers/app-reducer";
 
 const App = () => {
- const d = useDispatch();
- const { isInit } = useSelector(({ app }) => ({
-  isInit: app.isInit,
- }));
+  const d = useDispatch();
+  const { isInit } = useSelector(({ app }) => ({
+    isInit: app.isInit,
+  }));
 
- useEffect(() => {
-  setTimeout(() => d(setInit(true)), 200);
-  return clearTimeout(() => d(setInit(true)), 200);
- }, []);
+  useEffect(() => {
+    setTimeout(() => d(setInit(true)), 200);
+    return clearTimeout(() => d(setInit(true)), 200);
+  }, []);
 
- return (
-  <div className="app_content">
-   <Navbar />
-   <Hero />
-   <Skills isInit={isInit} />
-   <Projects />
-   <MainLoader isInit={isInit} />
-  </div>
- );
+  return (
+    <div className="app_content">
+      <Navbar />
+      <Hero />
+      <Skills isInit={isInit} />
+      <Projects />
+      <MainLoader isInit={isInit} />
+    </div>
+  );
 };
 
 export default App;
